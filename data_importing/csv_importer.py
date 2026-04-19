d9af9589f400d4adfb35a7ba63d88178a37c5e18
Rlastre <lastreronald7@gmail.com>
2026-04-18 23:55:10 -0400

    updated csv imports and the test to reflect the formatting expected from db.py

--- data_importing/csv_importer.py
+++ data_importing/csv_importer.py
@@ -1,14 +1,14 @@
 import sqlite3
 import pandas as pd
 from pathlib import Path
-
+from debt import Debt
 
 # will read from csv file and return the neccesary data in a list of dicts
 # CSV should be in format: Debt Name,Account Type,Current Balance,Interest Rate (%),Minimum Payment,Due Date
 # examples:
 #     Chase Credit Card,Credit Card,1500.00,19.99,50.00,15
 #     Student Loan A,Loan,12000.00,4.5,150.00,1
-def read_csv(file: str) -> list[dict]:
+def read_csv(file: str) -> list[Debt]:
     #parent folder
     base_path = Path(__file__).parent
 
@@ -20,8 +20,19 @@ def read_csv(file: str) -> list[dict]:
         print("File not found:", full_path)
         return
     data = pd.read_csv(full_path)
-    all_debts = data.to_dict(orient="records")
-    return all_debts
+    debts = [
+        Debt(
+            name=row["Debt Name"],
+            account_type=row["Account Type"],
+            balance=row["Current Balance"],
+            interest=row["Interest Rate (%)"],
+            min_payment=row["Minimum Payment"],
+            due_date=row["Due Date"]
+        )
+        for _, row in data.iterrows()
+    ]
+
+    return debts
 
 
 
--- data_importing/data_importing_test.py
+++ data_importing/data_importing_test.py
@@ -1,31 +1,28 @@
 import sqlite3
-from csv_importer import *
-from database_functions import *
+from data_importing.csv_importer import *
 from debt import *
+from db import *
 
 #tester showing how the functions might be used
 if __name__ == "__main__":
     #create connection to db (making a temperary file for testing purposes)
-    conn = sqlite3.connect(":memory:")
-
+    db:DatabaseManager = DatabaseManager(":memory:")
     #create table
-    print("Creating table")
-    create_tables(conn)
+    db.initialize_schema()
+    userId = db.create_user("test_user", "password123")
 
     #read data
-    print("Reading debt from csv")
     debts = read_csv("debt_test_data.csv")
     
     #save debts to db table
-    print("Saving Debts")
-    save_debt(conn, debts)
-
+    for d in debts:
+        db.add_debt(userId,d.account_type, d.balance, d.interest, d.min_payment, d.due_date)
+    
     #get data from table
     print("Getting all the debts in the table")
-    table_debts = get_all_debts(conn)
+    # table_debts = get_all_debts(conn)
 
     #from the list of the debts print the summary
-    for d in table_debts:
-        d.print_summary()
-
-    conn.close()
+    user_debts = db.get_debts_by_user(userId)
+    for debt in user_debts:
+        print(debt.summary())
