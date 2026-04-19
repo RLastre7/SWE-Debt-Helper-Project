8cc388af04f078e0ed33d7046717155ad4739fe0
RLastre7 <lastreronald7@gmail.com>
2026-04-19 09:33:32 -0400

    Refactor Debt model and DatabaseManager to align field names with database schema

--- debt.py
+++ debt.py
@@ -1,24 +1,22 @@
 class Debt:
-    def __init__(self, name, account_type, balance, interest, min_payment, due_date):
-        self.name = name
-        self.account_type = account_type
-        self.balance = balance
-        self.interest = interest
-        self.min_payment = min_payment
+    def __init__(self, creditor_name,current_balance, interest_rate, minimum_payment, due_date):
+        self.creditor_name = creditor_name
+        self.current_balance = current_balance
+        self.interest_rate = interest_rate
+        self.minimum_payment = minimum_payment
         self.due_date = due_date
 
     @classmethod
     def from_dict(cls, data: dict):
         return cls(
-            name=data["debt_name"],
-            account_type=data["account_type"],
-            balance=data["current_balance"],
-            interest=data["interest_rate"],
-            min_payment=data["minimum_payment"],
+            creditor_name=data["debt_name"],
+            current_balance=data["current_balance"],
+            interest_rate=data["interest_rate"],
+            minimum_payment=data["minimum_payment"],
             due_date=data["due_date"]
         )
     
     #prints a summary of the debt (Name: balance)
     def print_summary(self):
-        print(f"{self.name}: {self.balance}")
+        print(f"{self.creditor_name}: {self.current_balance}")
 
